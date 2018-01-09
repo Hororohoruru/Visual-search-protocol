d997410d15c3f777040ea40bb9d20858df9e8744
Juan Jesús Torre Tresols <juanjesustorre@gmail.com>
2018-01-09 10:31:38 +0100

    Add files via upload

---
+++ data_analysis/averages.m
@@ -0,0 +1,6 @@
+%Average reaction times for pop search
+[avg_1_pop, avg_0_pop] = rt_average(pop_4, pop_8, pop_12, pop_16);
+
+%Average reaction times for conjunction search
+
+[avg_1_conj, avg_0_conj] = rt_average(conj_4, conj_8, conj_12, conj_16);
